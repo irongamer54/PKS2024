d102256601e7a3152705609ccb6b7ba64cd9f03c
irongamer54 <61191096+irongamer54@users.noreply.github.com>
2024-01-14 18:00:38 +0700

    Update Zdch_5.ino

--- 2_typ/Zdch_5/Zdch_5.ino
+++ 2_typ/Zdch_5/Zdch_5.ino
@@ -1,17 +1,17 @@
 #define T_PIN PA0 //A0
 
-#define RES 10000 // сопротивление попутного резистора
-#define T0 25 // номинальная температура
-#define T_RES0 10000 // сопротивление терморезистора при T0
-#define B_COF 3650 // Бета коэфицент
-#define V_IN 5 //напряжение на терморезисторе
+#define RES 10000.0 // сопротивление попутного резистора
+#define T0 25.0 // номинальная температура
+#define T_RES0 10000.0 // сопротивление терморезистора при T0
+#define B_COF 3650.0 // Бета коэфицент
+#define V_IN 5.0 //напряжение на терморезисторе
 
 #define DEL 500 // задержка нагрева
 
 float get_temp(){ //получаем температуру
     float v_out = map(analogRead(T_PIN),0,4095,0,3.3); // падение напряжения на терморезисторе
     float r_term = (T_RES0 * v_out)/(V_IN-v_out);
-    float temp = B_COF * (T0 + 273,15) / (B_COF + log(float(r_term / T_RES0)) * (T0 + 273,15));
+    float temp = B_COF * (T0 + 273,15) / (B_COF + log(float(r_term / T_RES0)) * (T0 + 273.15));
     return temp-273.15;
 }
 
